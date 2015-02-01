1b970d7385d91e77e4438656e71e80f7b9963df3
BillHHHuang <BillHuangg@gmail.com>
2015-02-01 15:46:37 +0800

    Add Leap dll

--- Leap4EProject/Leap4EProject/MainWindow.xaml.cs
+++ Leap4EProject/Leap4EProject/MainWindow.xaml.cs
@@ -12,14 +12,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Leap;
+using Leap4EProject.TestPart;
 
 namespace Leap4EProject {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : Window {
+        private TestListener listener;
+        private Controller controller;
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            // Set leap controller
+            // Create a sample listener and controller
+            this.listener = new TestListener();
+            this.listener.OnFrameEvent += listener_OnFrameEvent;
+            this.controller = new Controller();
+
+            // Have the sample listener receive events from the controller
+            this.controller.AddListener(this.listener);
+        }
+
+        void listener_OnFrameEvent(object sender, EventArgs e) {
+            this.Dispatcher.BeginInvoke(new Action(delegate {
+                //this.LeapStateLabel.Content = "connected";
+            }), null);
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e) {
+            // Remove the sample listener when done
+            this.controller.RemoveListener(this.listener);
+            this.controller.Dispose();
         }
     }
 }
